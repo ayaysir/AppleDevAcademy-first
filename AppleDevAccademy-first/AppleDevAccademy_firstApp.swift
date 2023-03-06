e763a07e6bca8e00e599d5ba53553366f4dd2c20
BGSMM <yoonbumtae@gmail.com>
2023-03-06 12:39:05 +0900

    Initial Commit

---
+++ AppleDevAccademy-first/AppleDevAccademy_firstApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleDevAccademy_firstApp.swift
+//  AppleDevAccademy-first
+//
+//  Created by 윤범태 on 2023/03/06.
+//
+
+import SwiftUI
+
+@main
+struct AppleDevAccademy_firstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
