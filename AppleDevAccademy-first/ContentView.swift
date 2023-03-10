812b4e454d748919603508b4b97bcbb6483655a6
BGSMM <yoonbumtae@gmail.com>
2023-03-11 03:03:43 +0900

    SwiftIUI Practice #1 - designed music player layout

--- AppleDevAccademy-first/ContentView.swift
+++ AppleDevAccademy-first/ContentView.swift
@@ -8,12 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var statusText = "Ready to play..."
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+            // 이미지 사이즈 조정
+            Image("Adiemus II")
+                .resizable()
+                .frame(width: 380, height: 380)
+            Spacer()
+            
+            // 각종 버튼 (HStack)
+            HStack {
+                Spacer()
+                Button {
+                    statusText = "Backward"
+                } label: {
+                    Image(systemName: "backward.end.fill")
+                        .font(.system(size: 50))
+                        .foregroundColor(.black)
+                }
+                Spacer()
+                Button {
+                    statusText = "Play"
+                } label: {
+                    Image(systemName: "play.fill")
+                        .font(.system(size: 50))
+                        .foregroundColor(.black)
+                }
+                Spacer()
+                Button {
+                    statusText = "Afterward"
+                } label: {
+                    Image(systemName: "forward.end.fill")
+                        .font(.system(size: 50))
+                        .foregroundColor(.black)
+                }
+                Spacer()
+            }
+            // Spacer 높이 변경
+            Spacer().frame(height: 25)
+            Text(statusText)
+                .foregroundColor(.gray)
+            Spacer()
         }
         .padding()
     }
